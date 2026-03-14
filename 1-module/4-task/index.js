1028ec837a7086d4058a9eba509b4e1cc6a14a25
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-14 14:33:03 +0300

    Done 1-module-5-task and added tests for 3-4-tasks in module-1

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -16,3 +16,24 @@ console.log(checkSpam('free xxxxx'));
 
 console.log(checkSpam('innocent rabbit')); 
 // false
+
+//Позитивные случаи (спам найден)
+console.log(checkSpam('XXX content here'));   // true
+console.log(checkSpam('Some 1xBet offer'));   // true
+console.log(checkSpam('xxx and 1XBET'));      // true
+
+//Граничные случаи (слово в начале, конце, смешанный регистр)
+console.log(checkSpam('1xbet'));              // true
+console.log(checkSpam('xxx'));                // true
+console.log(checkSpam('XxX'));                // true
+console.log(checkSpam('Text before 1XBET'));  // true
+console.log(checkSpam('Text after XXX'));     // true
+
+//Отрицательные случаи (спама нет)
+console.log(checkSpam('1xbetting'));          // false (часть слова, не спам)
+console.log(checkSpam('hello world'));        // false
+console.log(checkSpam('XX'));                 // false (не полное "XXX")
+console.log(checkSpam(''));                   // false (пустая строка)
+
+//Очень длинная строка без спама
+console.log(checkSpam('Lorem ipsum dolor sit amet, consectetur adipiscing elit.')); // false
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,43 @@
 function truncate(str, maxlength) {
-  // ваш код...
+
+  if (str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + "…";
+  }
+
+  return str;
 }
+
+console.log(
+  truncate('Вот, что мне хотелось бы сказать на эту тему:', 20)
+);
+// Вот, что мне хотело…
+
+console.log(
+  truncate('Всем привет!', 20)
+);
+// Всем привет!
+
+//Позитивные случаи
+console.log(truncate("Привет", 10));         // "Привет"
+console.log(truncate("JavaScript", 10));     // "JavaScript"
+
+//На границе (строка длиннее лимита на 1 символ)
+console.log(truncate("12345", 4));           // "123…"
+console.log(truncate("abcd", 3));            // "ab…"
+
+//Немного за границей (строка на 2–3 символа больше лимита)
+console.log(truncate("abcdef", 4));          // "abc…"
+console.log(truncate("1234567", 5));         // "1234…"
+
+//Сильно за границей (очень длинные строки)
+console.log(truncate("Lorem ipsum dolor sit amet", 10)); // "Lorem ips…"
+console.log(truncate("Очень длинная строка для теста", 15)); // "Очень длинная …"
+
+//Граничные и маленькие лимиты
+console.log(truncate("Привет", 1));          // "…"
+console.log(truncate("A", 1));               // "A"
+console.log(truncate("AB", 1));              // "…"
+console.log(truncate("", 0));                // ""
+
+//Строка с пробелами
+console.log(truncate("  JavaScript  ", 5));  // "  Ja…"
