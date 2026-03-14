d81b20d31fe185891f73a71a6e1e33aae43aef6d
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-14 14:12:27 +0300

    Done 1-module-3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,19 @@
 function ucFirst(str) {
-  // ваш код...
+
+  if (str === '') {
+    return '';
+  }
+
+  return str[0].toUpperCase() + str.slice(1);
 }
+
+console.log(ucFirst('вася')); // Вася
+console.log(ucFirst('в'));    // В
+console.log(ucFirst(''));
+console.log(ucFirst('Вася')); // Вася
+console.log(ucFirst('вася123')); // Вася123
+console.log(ucFirst('вася!')); // Вася!
+console.log(ucFirst('вася?')); // Вася?
+console.log(ucFirst('вася.')); // Вася.
+console.log(ucFirst('вася,')); // Вася,
+console.log(ucFirst('даша ')); // Даша 
