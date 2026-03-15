609b0dd59c48c18cc61d5ab2774c7d911d5f36b4
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-15 15:18:11 +0300

    Done 3-module-2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,9 @@
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(item => item >= a && item <= b);
 }
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+
+console.log(filtered); // [3, 1]
+console.log(arr);      // [5, 3, 8, 1] — исходный массив не изменился
