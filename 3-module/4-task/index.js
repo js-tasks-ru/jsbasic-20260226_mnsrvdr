2c8059d53409f3ba660b2510ff21489127876238
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-15 15:25:16 +0300

    Done 3-module-4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,31 @@
 function showSalary(users, age) {
-  // ваш код...
+  return users
+    .filter(user => user.age <= age)
+    .map(user => `${user.name}, ${user.balance}`)
+    .join('\n');
 }
+
+// Пример использования:
+let users = [
+  {
+    "balance": "$1,825.65",
+    "age": 21,
+    "name": "Golden Branch"
+  },
+  {
+    "balance": "$1,490.15",
+    "age": 25,
+    "name": "Duncan Randall"
+  },
+  {
+    "balance": "$2,500.00",
+    "age": 30,
+    "name": "Alice Cooper"
+  }
+];
+
+console.log(showSalary(users, 25));
+/* Вывод:
+Golden Branch, $1,825.65
+Duncan Randall, $1,490.15
+*/
