9939b833da374539379f01136342799eaec1328a
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-15 15:12:18 +0300

    Done 3-module-1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,13 @@
 function namify(users) {
-  // ваш код...
+  return users.map(user => user.name);
 }
+
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = namify(users);
+
+console.log(names); // ['Вася', 'Петя', 'Маша']
