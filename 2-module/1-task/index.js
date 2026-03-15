56ff716e4f5098ce6527851e02a1d2ef00a0c1d7
alonskaya-ctrl <mnsrvdr@gmail.com>
2026-03-15 12:30:42 +0300

    Done 2-module-1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,42 @@
 function sumSalary(salaries) {
-  // ваш код...
+
+  let sum = 0;
+
+  for (let key in salaries) {
+
+    let value = salaries[key];
+
+    if (
+      typeof value === "number" &&
+      value === value &&
+      value !== Infinity &&
+      value !== -Infinity
+    ) {
+      sum += value;
+    }
+
+  }
+
+  return sum;
 }
+
+//проверка
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
+console.log(sumSalary(salaries));
+
+//корнер кейсы
+
+//let salaries = {}//0
+
+//let salaries = {currency: "USD", isPayed: true}) //0
+
+//let salaries = {John: 1000, Ann: NaN, Pete: Infinity}) //1000
