ddef78d7222157f6f9d5f9aa54144d2a9060eda9
daria.minisiyarova <daria.minisiyarova@1win.pro>
2026-04-11 15:04:01 +0300

    Fixing 6-2

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,29 +1,37 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
     this.product = product;
-    this.elem = document.createElement('div');
-    this.elem.className = 'card';
-    this.elem.innerHTML = `
-      <div class="card__top">
-        <img src="../assets/images/products/${product.image}" class="card__image" alt="product">
-        <span class="card__price">€${product.price.toFixed(2)}</span>
-      </div>
-      <div class="card__body">
-        <div class="card__title">${product.name}</div>
-        <button type="button" class="card__button">
-          <img src="../assets/images/icons/plus-icon.svg" alt="icon">
-        </button>
-      </div>
-    `;
+    this.elem = this.render();
+  }
 
-    this.elem.addEventListener('click', (event) => {
-      if (event.target.closest('.card__button')) {
-        let customEvent = new CustomEvent("product-add", {
-          detail: { id: this.product.id },
-          bubbles: true
-        });
-        this.elem.dispatchEvent(customEvent);
-      }
+  render() {
+    const { name, price, image } = this.product;
+
+    const priceFormatted = Number(price).toFixed(2);
+    const imagePath = `/assets/images/products/${image}`;
+
+    const card = createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img src="${imagePath}" class="card__image" alt="product">
+          <span class="card__price">€${priceFormatted}</span>
+        </div>
+        <div class="card__body">
+          <div class="card__title">${name}</div>
+          <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+      `);
+
+    const button = card.querySelector('.card__button');
+    button.addEventListener('click', () => {
+      card.dispatchEvent(new CustomEvent('product-add', { detail: this.product.id, bubbles: true }));
     });
+
+    return card;
   }
 }
